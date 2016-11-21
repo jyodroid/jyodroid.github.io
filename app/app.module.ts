c1e1fb012df3015d5115fcb10147f5c52c9bd37f
John Tangarife <jyodroid@gmail.com>
2016-11-20 23:00:05 -0500

    Route class and params

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,30 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }   from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { HeroDetailComponent } from './modules/hero-detail/hero-detail.component';
+import { HeroesComponent } from './modules/heroes.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { HeroService } from './services/hero.service';
+
+import { AppRoutingModule }     from './modules/app-routing.module';
 
 @NgModule({
   imports:      [
                   BrowserModule,
-                  FormsModule
+                  FormsModule,
+                  AppRoutingModule
                 ],
   declarations: [
                   AppComponent,
-                  HeroDetailComponent
+                  HeroDetailComponent,
+                  HeroesComponent,
+                  DashboardComponent
                 ],
+  providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
---
+++ app/modules/hero-detail/hero-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero } from '../../models/hero';
+import { HeroService } from '../../services/hero.service';
+
+import 'rxjs/add/operator/switchMap';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  templateUrl: 'hero-detail.component.html'
+})
+export class HeroDetailComponent implements OnInit {
+
+  // by declaring de decorated input anotation we are binding attribute with
+  // the caller parent module (component)
+  @Input()
+  hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
