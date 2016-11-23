2ecb98c11254b477a1988ff742af0fe81e3fa981
John Tangarife <jyodroid@gmail.com>
2016-11-23 02:43:11 -0500

    dist example page

--- app/main.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-import {enableProdMode} from '@angular/core';
-const platform = platformBrowserDynamic();
-// enableProdMode();
-if (process.env.ENV === 'production') {
-  enableProdMode();
-}
-platform.bootstrapModule(AppModule);
